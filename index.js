f49639803bb119f325a895352c65fc540312779b
Todd Krieger <twkriege@mtu.edu>
2021-09-20 23:23:50 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(fun = "roller-skate") {
+return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(accent = '*') {
+    return function (emphatic) {
+        return `You are ${accent}${emphatic}${accent}!`;
+    }
+}
